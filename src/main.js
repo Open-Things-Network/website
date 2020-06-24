8ea2e28c9120247b4475f9b3f8f93ee79290a4da
Grzegorz Skorupa <GSkorupa@nmg.pl>
2020-06-25 01:49:50 +0200

    wersja angielska - początek

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ let app = new App({
     props: {
         devModePort: '5000',
         defaultLanguage: 'pl',
-        languages: ['pl'], 
-        //languages:['pl','en'],
+        //languages: ['pl'], 
+        languages:['pl','en'],
         language: 'pl',
         cmsMode: false
     }
